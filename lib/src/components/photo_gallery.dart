56dde34a0168a89b7977ec829d83b4f787ca32bc
hhjeee <yoonhj0315@gmail.com>
2024-02-18 04:00:37 +0900

    modify: photo gallery

--- lib/src/components/photo_gallery.dart
+++ lib/src/components/photo_gallery.dart
@@ -1,95 +1,26 @@
 import 'package:flutter/material.dart';
-import 'dart:io';
-
-//import 'package:google_ml_kit/google_ml_kit.dart';
-import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 
 class PhotoGallery extends StatefulWidget {
-  final String searchKeyword;
-  final List<String> imagePaths;
-
-  PhotoGallery({Key? key, this.searchKeyword = '', required this.imagePaths})
-      : super(key: key);
+  PhotoGallery({super.key});
 
   State<PhotoGallery> createState() => _PhotoGalleryState();
 }
 
 @override
 class _PhotoGalleryState extends State<PhotoGallery> {
-  /*final List<String> thumbnailUrls = [
+  final String originalPhotoUrl = 'assets/images/ex1.jpg';
+  final List<String> thumbnailUrls = [
     'assets/images/ex1.jpg',
     'assets/images/ex2.jpg',
     'assets/images/ex3.jpg',
-  ];*/
+  ];
 
-  int shownindex = 0;
-  bool isKeywordFound = false;
-  List<Rect> textRects = [];
-
-  @override
-  void initState() {
-    super.initState();
-    if (widget.searchKeyword.isNotEmpty) {
-      performSearch(widget.searchKeyword);
-    }
-  }
-
-  @override
-  void didUpdateWidget(oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (widget.searchKeyword != oldWidget.searchKeyword) {
-      // 검색 키워드 변경
-      print('bbbbbb');
-      print(widget.searchKeyword);
-      performSearch(widget.searchKeyword);
-    }
-  }
+  var shownindex = 0;
 
   void Tapped(index) {
     setState(() {
       shownindex = index;
     });
-    //  performSearch(widget.searchKeyword);
-  }
-
-  Future<void> performSearch(String query) async {
-    if (query.isEmpty) return;
-
-    final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
-
-    for (String imagePath in widget.imagePaths) {
-      final File imageFile = File(imagePath);
-      final inputImage = InputImage.fromFile(imageFile);
-      final RecognizedText recognizedText =
-          await textRecognizer.processImage(inputImage);
-
-      bool keywordFound = false;
-      for (TextBlock block in recognizedText.blocks) {
-        for (TextLine line in block.lines) {
-          if (line.text.contains(query)) {
-            keywordFound = true;
-            textRects.add(line.boundingBox);
-            print('aaaaaa');
-            print(line.boundingBox);
-            break;
-          }
-        }
-        if (keywordFound) break;
-      }
-
-      if (keywordFound) {
-        if (mounted) {
-          setState(() {
-            isKeywordFound = true;
-            shownindex = widget.imagePaths.indexOf(imagePath);
-          });
-        }
-        print("$imagePath에서 '$query' 키워드를 찾았습니다.");
-        break;
-      }
-    }
-
-    textRecognizer.close();
   }
 
   @override
@@ -102,25 +33,6 @@ class _PhotoGalleryState extends State<PhotoGallery> {
           children: <Widget>[
             Container(
               padding: EdgeInsets.all(10.0),
-              child: isKeywordFound
-                  ? CustomPaint(
-                      painter: TextHighlightPainter(textRects),
-                      // 여기서 textRects는 OCR에서 인식한 텍스트의 위치를 나타내는 Rect의 리스트입니다.
-                      child: Image.file(
-                        File(widget.imagePaths[shownindex]),
-                        width: 400,
-                        height: 500,
-                        fit: BoxFit.cover,
-                      ),
-                    )
-                  : Image.file(
-                      File(widget.imagePaths[shownindex]), // Image.file 사용
-                      width: 400,
-                      height: 500,
-                      fit: BoxFit.cover,
-                    ),
-            ),
-            /*
               child: Image.asset(
                 thumbnailUrls[shownindex],
                 width: 400,
@@ -134,10 +46,9 @@ class _PhotoGalleryState extends State<PhotoGallery> {
                 onTap: () {},
                 child: const Icon(Icons.link, color: Colors.white),
               ),
-            )*/
+            )
           ],
         ),
-        /*
         Container(
           height: 80,
           child: ListView.builder(
@@ -152,41 +63,15 @@ class _PhotoGalleryState extends State<PhotoGallery> {
                     padding: const EdgeInsets.all(8.0),
                     child: Image.asset(
                       thumbnailUrls[index],
-                      width: 60, // Adjust the width as needed
-                      height: 80, // Adjust the height as needed
+                      width: 60,
+                      height: 80,
                       fit: BoxFit.cover,
                     ),
                   ));
             },
           ),
-        ),
-     */
+        )
       ]),
     );
-  }
-}
-
-// CustomPainter를 상속받는 TextHighlightPainter 클래스
-class TextHighlightPainter extends CustomPainter {
-  final List<Rect> textRects;
-
-  TextHighlightPainter(this.textRects);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = Colors.yellow.withOpacity(0.3)
-      ..style = PaintingStyle.fill;
-
-    // 리스트에 있는 모든 Rect를 순회하며 캔버스에 그림
-    for (var rect in textRects) {
-      canvas.drawRect(rect, paint);
-    }
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    // CustomPainter가 다시 그려야 하는지 여부를 결정
-    return true;
   }
 }
